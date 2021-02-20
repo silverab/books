76753123068a6b5535ed69f7d3c88513bbec2ad2
Hassan <mhas4908@gmail.com>
2021-02-20 06:05:39 +0500

    'ch9'

--- silverab/settings.py
+++ silverab/settings.py
@@ -164,6 +164,7 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
 ACCOUNT_SESSION_REMEMBER = True
@@ -172,3 +173,5 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
+
+DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
